e45154f7142e626c59fd2b6c88ed004faa3444bf
samuelevi87 <samuel.levi.alves@outlook.com>
2022-01-10 17:27:45 -0300

    Project Created

---
+++ backend/src/main/java/com/sl3v1/slaamovies/SlaaMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.sl3v1.slaamovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaaMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlaaMoviesApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/sl3v1/slaamovies/SlaaMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl3v1.slaamovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaaMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
