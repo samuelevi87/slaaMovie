1b2918186f11ed66a988b95b0b38018d827b678f
samuelevi87 <samuel.levi.alves@outlook.com>
2022-01-14 17:10:35 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    // Forma errada
+
+    axios.get(`${BASE_URL}/movies?size=10&page=2`)
+            .then(response => {
+                console.log(response.data);
+                
+            });
 
     return (
         <>
